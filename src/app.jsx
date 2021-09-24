b2f36d821e90ed61e56a6f140d8ffa71f5a66fb1
heesung-H <hisky90@gmail.com>
2021-09-24 12:29:00 +0900

    add login component

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './app.css';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import Login from './components/login/login';
+import styles from './app.module.css';
 
 function App({authService}) {
   return (
-    <Login authService={authService}/>
-  )
+    <div className={styles.app}>
+      <Login authService={authService}/>
+    </div>
+  );
 }
 
 export default App;
