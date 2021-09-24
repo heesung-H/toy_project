516ef47bd76b47783a66f367f8693b4845319b1f
heesung-H <hisky90@gmail.com>
2021-09-24 13:13:49 +0900

    add Login and Logout

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({authService}) => {
+    
+    const history = useHistory();
+    const goToMaker = (userId) => {
+        history.push({
+            pathname : '/maker',
+            state: { id: userId }
+        });
+    };
+
     const onLogin = event => {
         authService
         .login(event.currentTarget.textContent)
+        .then(data => goToMaker(data.user.uid))
         .catch(error => {
-        })
+        });
     };
+
+    useEffect(() => {
+        authService
+        .onAuthChange(user => {
+            user && goToMaker(user.uid);
+        });
+    });
 
     return (        
         <section className={styles.login}>
