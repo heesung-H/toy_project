21c300d8c52c0f418b22492332a41ac6f2ee02de
heesung-H <hisky90@gmail.com>
2021-09-29 06:17:57 +0900

    add loading spinner

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRef } from 'react/cjs/react.development';
 import styles from './image_file_input.module.css'
 
 const ImageFileInput = ({imageUploader, name, onFileChange }) => 
 {
-
+    const [loading, setLoading] = useState(false);
     const inputRef = useRef();
     const onButtonClick = (event) => {
         event.preventDefault();
@@ -13,8 +13,9 @@ const ImageFileInput = ({imageUploader, name, onFileChange }) =>
 
     //파일 업로드
     const onChange = async (event) => {
+        setLoading(true);
         const uploaded = await imageUploader.upload(event.target.files[0]);
-        console.log(uploaded);
+        setLoading(false);
         onFileChange({
             name: uploaded.original_filename,
             url: uploaded.url
@@ -24,7 +25,8 @@ const ImageFileInput = ({imageUploader, name, onFileChange }) =>
     return (
         <div className={styles.container}>
             <input ref={inputRef} className={styles.input} type="file" accept="image/*" name="file" onChange={onChange} />
-            <button className={styles.button} onClick={onButtonClick}>{name || 'No file'}</button>
+            { !loading && <button className={`${styles.button} ${name ? styles.pink : styles.gray}`} onClick={onButtonClick}>{name || 'No file'}</button>}
+            { loading && <div className={styles.loading}></div>}
         </div>
     );
 }
