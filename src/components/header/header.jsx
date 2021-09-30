fc745a60cbc3de7095ef2fb4cfec7737822a67ad
heesung-H <hisky90@gmail.com>
2021-09-30 17:49:22 +0900

    Last Days

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ const Header = memo(({onLogout, onLink}) =>
 {
     return(
         <header className={styles.header}>
-            <button onClick={onLink} className={styles.recycle}><img className={styles.thumbnail} src="/images/recycle.png" alt="recycle"/></button>
+            {onLogout && (<button onClick={onLink} className={styles.recycle}><img className={styles.thumbnail} src="/images/recycle.png" alt="recycle"/></button>)}
             {onLogout && (<button className={styles.logout} onClick={onLogout}>Logout</button>)}
             <img className={styles.logo} src="/images/logo.png" alt="logo"/>
             <h1 className={styles.title}>Project Toy</h1>
